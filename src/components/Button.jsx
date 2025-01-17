2907b7adea700dfdc79ee57cc971f5c9f7b56f91
unknown <connor.wilson48@gmail.com>
2025-01-17 14:14:19 -0800

    Changed button scroll behavior to smooth and fixed minor spelling mistake

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Button = ({ text, onClick }) => {
+const Button = ({ text, location}) => {
+
+  const handleClick = (e) => {
+    e.preventDefault(); 
+    const section = document.querySelector(location);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   return (
     <a
-      href={onClick}
+      onClick={handleClick}
       className="px-6 py-3 mt-8 bg-customTeal text-white font-semibold rounded-lg shadow-md 
              hover:bg-white hover:text-customTeal border-2 border-customTeal 
              transition duration-300"
