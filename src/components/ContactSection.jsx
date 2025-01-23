a627c51a2c91b977fd5a2e73b18b653d0015dedd
unknown <connor.wilson48@gmail.com>
2025-01-22 21:38:58 -0800

    modified incorrect phone number

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -7,7 +7,7 @@ const ContactSection = () => {
         <h1 className="text-3xl font-bold text-customTeal mb-4">Contact Us</h1>
         <div className="flex flex-col md:flex-row items-center justify-center gap-8">
           <p className="text-lg text-gray-600">
-            📞 <a href="tel:3608536806" className="hover:underline">360-853-6806</a>
+            📞 <a href="tel:3608536808" className="hover:underline">360-853-6808</a>
           </p>
           <p className="text-lg text-gray-600">
             ✉️ <a href="mailto:bowman.dpt@gmail.com" className="hover:underline">bowman.dpt@gmail.com</a>
