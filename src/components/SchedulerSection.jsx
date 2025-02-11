c167306d98d1bd117f2997b38c9cda0bbf75f9e3
unknown <connor.wilson48@gmail.com>
2025-02-10 21:51:41 -0800

    Updates to calendar functionality

--- src/components/SchedulerSection.jsx
+++ src/components/SchedulerSection.jsx
@@ -1,12 +1,14 @@
 'use client';
 import { useAuth } from "../lib/useAuth";
+import Calendar from "./Calendar";
 import React from "react";
 
 export const Scheduler = () => {
     
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100 px-4 sm:px-6">
-            <h1 className="text-center text-2xl font-bold text-gray-800 mb-4">scheduler </h1>
+            <h1 className="text-center text-2xl font-bold text-gray-800 mb-4">Schedule </h1>
+            <Calendar/>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-    <App />
-    </Router>
+      <Router>
+        <App />
+      </Router>
   </React.StrictMode>
 );
 
