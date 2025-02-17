1fe8577132c3b9d751592e3c4b5fce071e6919da
unknown <connor.wilson48@gmail.com>
2025-02-16 17:51:37 -0800

    Created functionality for adding times/availabilty

---
+++ src/lib/create.js
@@ -0,0 +1,17 @@
+import { collection, addDoc} from 'firebase/firestore';
+import { db } from './firebase';
+
+export const addAvailability = async (date) => {
+    await addDoc(collection(db, "availability"), {
+        address: "",
+        booked: false,
+        city: "",
+        email: "",
+        firstName: "",
+        lastName: "",
+        notes: "",
+        state: "",
+        time: date,
+        zipCode: "",
+      });
+}
--- src/lib/reads.js
+++ src/lib/reads.js
@@ -1,4 +1,4 @@
-import { collection, getDocs, doc, getDoc, query, where } from 'firebase/firestore';
+import { collection, getDocs, doc, getDoc, query, where, QuerySnapshot } from 'firebase/firestore';
 import { db } from './firebase';
 import { Timestamp } from "firebase/firestore";
 
@@ -8,16 +8,53 @@ export const userIsAdmin = async (uid) => {
 
 const availabilityRef = collection(db, "availability");
 
+export const getAvailableDays = async()=> {
+
+    const availabilityRef = collection(db, "availability");
+
+    try {
+        const querySnapshot = await getDocs(availabilityRef); // Use predefined availabilityRef
+    
+        const days = querySnapshot.docs.map(doc => {
+          const date = doc.data().time.toDate(); // Convert Firestore Timestamp to JavaScript Date
+    
+          const year = date.getFullYear();
+          const month = String(date.getMonth() + 1).padStart(2, "0"); // Ensure 2-digit month
+          const day = String(date.getDate()).padStart(2, "0"); // Ensure 2-digit day
+    
+          return `${year}-${month}-${day}`; // Format as "YYYY-MM-DD"
+        });
+
+        /*const uniqueDays = days.filter(
+            (value, index, self) =>
+              index === self.findIndex((t) => t.day === value.day)
+          );*/
+
+          const uniqueDays = [...new Set(days)];
+
+          
+    
+        console.log("Formatted Times:", uniqueDays);
+        return uniqueDays;
+      } catch (error) {
+        console.error("❌ Error fetching times:", error);
+        return [];
+      }
+}
+
 export const getAvailableTimes = async (date) => {
     
     const startOfDay = Timestamp.fromDate(new Date(date.setHours(0, 0, 0, 0))); // 00:00:00
     const endOfDay = Timestamp.fromDate(new Date(date.setHours(23, 59, 59, 999))); // 23:59:59
     
+    
+    const availabilityRef = collection(db, "availability");
+
     const queryDate = new Date(date);
     queryDate.setHours(12,0,0,0).toLocaleString("en-US", { timeZone: "America/Los_Angeles" }); // set time to noon;
    
-    const availabilityRef = collection(db, "availability");
-    const q = query(availabilityRef, where("day", ">=", startOfDay), where("day", "<=", endOfDay));
+    
+    const q = query(availabilityRef, where("time", ">=", startOfDay), where("time", "<=", endOfDay));
     const snapshot = await getDocs(q);
 
     const times = snapshot.docs
