29e2731fe3bdb174a58bd8ad8c9e552a95f44426
unknown <connor.wilson48@gmail.com>
2025-02-19 15:59:44 -0800

    Changed date and time inputs to be on different lines. Added label next to both inputs

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -154,7 +154,11 @@ const CalendarSection = () => {
     await addAvailability(newDate);
 
     const dates = await getAvailableDays(); // Fetch from Firestore
-      setAvailableDates(dates); // Set state with available dates
+    setAvailableDates(dates); // Set state with available dates
+
+    const times = await getAvailableTimes(selectedDate);
+    
+    setAvailableTimes(times); 
     
   }
 
@@ -175,75 +179,90 @@ const CalendarSection = () => {
   };
 
   return (
-  <div className="calendar-container">
-              <Calendar
-                value={date}
-                onChange={setDate}
-                minDate={new Date()} // Disable past dates
-                defaultActiveStartDate={new Date()} // Load with the current month
-                onClickDay={handleDateClick}
-                onActiveStartDateChange={handleActiveStartDateChange} // Detect month changes
-                tileClassName={getTileClassName} // Apply custom styles
-                tileDisabled={isTileDisabled} // Disable unselectable dates
+    <div className="calendar-container">
+      <Calendar
+        value={date}
+        onChange={setDate}
+        minDate={new Date()} // Disable past dates
+        defaultActiveStartDate={new Date()} // Load with the current month
+        onClickDay={handleDateClick}
+        onActiveStartDateChange={handleActiveStartDateChange} // Detect month changes
+        tileClassName={getTileClassName} // Apply custom styles
+        tileDisabled={isTileDisabled} // Disable unselectable dates
+      />
+      <p className="text-2xl font-bold text-gray-800">
+        {new Date(selectedDate).toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" })}
+      </p>
+      <div className="availability-container">
+        <h2 className="text-2xl text-gray-800 mb-4">Availability:</h2>
+        <ul className="availability-list">
+          {availableTimes.length > 0 ? (
+            availableTimes.map(({ id, time }) => (
+              <li key={id} className="availability-item">
+                <div className="time-slot-container">
+                  <button className="time-slot-button" onClick={() => handleTimeSlotClick(id, time)}>
+                    {time}
+                    {user ?
+                      <span className="info-button" onClick={(e) => { e.stopPropagation(handleTimeSlotClickEdit(id, time)); }}>
+                        Edit
+                      </span> : ""}
+                  </button>
+                </div>
+                {user ?
+                  <button className="delete-button" onClick={() => deleteTime(id)}>
+                    X
+                  </button> : ""}
+              </li>
+            ))
+          ) : (
+            <p className="no-availability-text">No available times for this date.</p>
+          )}
+        </ul>
+        {user ? <>
+          <h1 className='mt-10 text-2xl text-gray-800 mb-4'>Add Availability:</h1>
+
+          <div className="datetime-picker-container">
+            {/* Date Picker on its own line */}
+            <div className="date-input-container">
+              <h2 className="label">Date:</h2>
+              <DatePicker
+                selected={selectedDatePick}
+                onChange={(date) => setSelectedDatePick(date)}
+                className="datepicker"
+                placeholderText="mm/dd/yyyy"
               />
-              <p className="selected-date-text">
-              <strong>{new Date(selectedDate).toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" })}</strong>
-              </p>
-              <div className="availability-container">
-                      <h2 className="availability-title">Availability:</h2>
-                      <ul className="availability-list">
-                      {availableTimes.length > 0 ? (
-                        availableTimes.map(({ id, time }) => (
-                        <li key={id} className="availability-item">
-                        <div className="time-slot-container">
-                        <button className="time-slot-button" onClick={() => handleTimeSlotClick(id, time)}>
-                          {time}
-                          {user ?
-                          <span className="info-button" onClick={(e) => { e.stopPropagation(handleTimeSlotClickEdit(id,time));  }}>
-                            Edit
-                          </span> : ""}
-                        </button>
-                        </div>
-                        {user ? 
-                        <button className="delete-button" onClick={() => deleteTime(id)}>
-                        X
-                        </button>: ""}
-                        </li>
-                      ))
-                      ) : (
-                      <p className="no-availability-text">No available times for this date.</p>
-                      )}
-                      </ul>
-                      {user ? <>
-                      <h1 className='mt-10'>Add Availability:</h1>
-                      
-                      <div className="date-time-container">
-                        <DatePicker 
-                        selected={selectedDatePick} 
-                        onChange={(date) => setSelectedDatePick(date)} 
-                        className="datepicker"
-                        />
-                        <input type="time" onChange={(e) => setTimePick(e.target.value)} className="timepicker"/>
-                      </div>
-                      <button className="add-button" onClick={() => addDate()}>
-                        Add
-                      </button> </> : ""}
-              </div>
-          <Modal
-          selectedDate={selectedDate}
-          selectedTime={selectedTime}
-          firstName={firstName} setFirstName={setFirstName}
-          lastName={lastName} setLastName={setLastName}
-          email={email} setEmail={setEmail}
-          address={address} setAddress={setAddress}
-          city={city} setCity={setCity}
-          state={state} setState={setState}
-          zipCode={zipCode} setZipCode={setZipCode}
-          notes={notes} setNotes={setNotes}
-          closeModal={closeModal}
-          handleSubmit={handleSubmit}
-          />
-</div>
+            </div>
+
+            {/* Time Picker on its own line */}
+            <div className="time-input-container">
+              <h2 className="label">Time:</h2>
+              <input
+               value={timePick}
+                type="time"
+                onChange={(e) => setTimePick(e.target.value)}
+                className="timepicker"
+              />
+            </div>
+          </div>
+          <button className="add-button" onClick={() => addDate()}>
+            Add
+          </button> </> : ""}
+      </div>
+      <Modal
+        selectedDate={selectedDate}
+        selectedTime={selectedTime}
+        firstName={firstName} setFirstName={setFirstName}
+        lastName={lastName} setLastName={setLastName}
+        email={email} setEmail={setEmail}
+        address={address} setAddress={setAddress}
+        city={city} setCity={setCity}
+        state={state} setState={setState}
+        zipCode={zipCode} setZipCode={setZipCode}
+        notes={notes} setNotes={setNotes}
+        closeModal={closeModal}
+        handleSubmit={handleSubmit}
+      />
+    </div>
   );
 };
 
